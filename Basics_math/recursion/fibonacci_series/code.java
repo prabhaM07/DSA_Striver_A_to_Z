e5ab9fc794137c1a67ce3ba727ee594c2169b24f
Prabha <prabha.m2022ai-ds@sece.ac.in>
2024-10-14 18:47:21 +0530

    fibonacci_series

---
+++ Basics_math/recursion/fibonacci_series/code.java
@@ -0,0 +1,59 @@
+package DSA_Striver_A_to_Z.Basics_math.recursion.fibonacci_series;
+import java.util.*;
+
+class code {
+    //METHOD 1
+    
+    public static void fibonacci1(int n){
+        int[] num = new int[n];
+        if(n<=1){
+            System.out.print(n);
+        }
+        else{
+            num[0]=0;
+            num[1] = 1;
+            for(int i=2;i<n;i++){
+                num[i] = num[i-1]+num[i-2];
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(num[i]+" ");
+        }
+    }
+
+    //METHOD 2
+
+    public static void fibonacci2(int n){
+        if(n<=1){
+            System.out.print(n);
+        }
+        else{
+            int s = 0;
+            int f = 1 ;
+            for(int i=0;i<n;i++){
+                int t = s+f;
+                System.out.print(s+" ");
+                s = f;
+                f=t;
+            }
+        }
+    }
+
+    //METHOD 3
+    public static int fibonacci3(int n){
+        if(n<=1){
+            return n;
+        }
+        
+        return fibonacci3(n-1)+fibonacci3(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci3(i) + " ");  
+        }
+    }
+
+
+}
---
+++ Basics_math/recursion/fibonacci_series/readme.md
@@ -0,0 +1,11 @@
+METHOD 1
+    Fibonacci series was printed using extra  space
+
+METHOD 2
+
+    Fibonacci series was printed without using extra space
+
+METHOD 3
+
+    Fibonacci series was printed using recursion
+
